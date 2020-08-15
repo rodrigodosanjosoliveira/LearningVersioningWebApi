87bc57d75f4fe9d57af21c8220b20678833a0bee
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2020-08-14 22:01:22 -0300

    Url based versioning

---
+++ LearningVersioningWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LearningVersioningWebApi.Controllers
+{
+    [ApiVersion("1")]
+    [Route("api/v{v:apiversion}/Values")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value one from api version", "One" };
+        }
+    }
+}
---
+++ LearningVersioningWebApi/Controllers/ValuesV2Controller.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LearningVersioningWebApi.Controllers
+{
+    [ApiVersion("2")]
+    [Route("api/v{v:apiversion}/Values")]
+    [ApiController]
+    public class ValuesV2Controller : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value two from api version", "Two" };
+        }
+    }
+}
